73543265f10b4591a990a232cb153a04e43be204
honghamin <113421639+honghamin@users.noreply.github.com>
2022-09-20 17:43:09 +0900

    03-04

--- main.03.c
+++ main.03.c
@@ -4,12 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	char c;
-	int i;
-	printf("input a number :");
-	scanf("%c", &c);
+	int i; 
+	int j;
 	
-	i = c-'0';
-	printf("The input number is %i\n", i);
+	printf("분자를 입력하세요:");
+	scanf("%d", &i);
+	
+	printf("분모를 입력하세요:");
+	scanf("%d", &j);
+	
+
+	printf("나누기의 결과는 %f 입니다\n", (float)i/j);
 	return 0;
 }
